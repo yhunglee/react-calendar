2ed40852d1bf9712c65e88d3dee41dd449ed7f09
Howard.Lee <howard@csie.io>
2021-08-29 23:52:08 +0800

    feat(MonthView.tsx): add layout for 12 months in a year.

--- src/components/Calendar/MonthView/MonthView.tsx
+++ src/components/Calendar/MonthView/MonthView.tsx
@@ -1,4 +1,57 @@
 /** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { NavigationBar } from "../NavigationBar/NavigationBar";
+
+const containerStyle = css`
+  width: 230px;
+`;
+
+const monthListStyle = css`
+  display: flex;
+  margin: auto 1rem;
+  flex-wrap: wrap;
+
+  > .element {
+    width: fit-content;
+    padding: 0.5rem;
+    margin: auto;
+
+    &:hover {
+      cursor: pointer;
+      background-color: #ccc;
+    }
+  }
+`;
+
 export const MonthView: React.VFC = (props) => {
-  return <div></div>;
+  let monthList = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  return (
+    <div className="container" css={containerStyle}>
+      <NavigationBar />
+
+      <div className="month-list" css={monthListStyle}>
+        {monthList.map((elem, idx) => {
+          return (
+            <div className="element" key={idx}>
+              {elem}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
