78b6c7224b1d56efe6a5419b623165a84e076acb
Howard.Lee <howard@csie.io>
2021-08-30 22:45:01 +0800

    feat(NavigationBar.tsx): add display info and actions for navigations.

--- src/components/Calendar/NavigationBar/NavigationBar.tsx
+++ src/components/Calendar/NavigationBar/NavigationBar.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { css } from "@emotion/react";
+import { NavigateAction } from "../types/NavigateActType";
 
 const infoActionStyle = css`
   display: flex;
@@ -26,15 +27,21 @@ const infoActionStyle = css`
   }
 `;
 
-export const NavigationBar: React.VFC = (props) => {
-  let info = "Aug, 2021"; // stubbed data
-
+export const NavigationBar: React.VFC<NavigateAction> = ({
+  prevAct,
+  nextAct,
+  info,
+}) => {
   return (
     <div className="info-action-bar" css={infoActionStyle}>
-      <span className="prev action">&lt;</span>
+      <span className="prev action" onClick={() => prevAct()}>
+        &lt;
+      </span>
 
       <div className="show-info">{info}</div>
-      <span className="next action">&gt;</span>
+      <span className="next action" onClick={() => nextAct()}>
+        &gt;
+      </span>
     </div>
   );
 };
