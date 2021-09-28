a451cdc507b7459da91bf5bb034cbede71a178d7
Howard.Lee <howard@csie.io>
2021-09-29 00:22:35 +0800

    feat(App, Calendar): add part feature of setting input and Calendar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Calendar from "./components/Calendar";
 
 function App() {
+  let [selectedDate, setSelectedDate] = useState("");
+
   return (
     <div className="App">
-      <Calendar.Calendar></Calendar.Calendar>
+      <input
+        type="date"
+        onChange={(e) => setSelectedDate(e.target.value)}
+        value={selectedDate}
+        pattern="\d{4}-\d{2}-\d{2}"
+      />
+      <Calendar.Calendar
+        date={selectedDate}
+        onSelect={setSelectedDate}
+      ></Calendar.Calendar>
     </div>
   );
 }
--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,15 +1,27 @@
 /** @jsxImportSource @emotion/react */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DateView } from "./DateView/DateView";
 import { MonthView } from "./MonthView/MonthView";
+import { CalendarType } from "./types/CalendarType";
 import { ViewKind } from "./types/ViewKind";
 import { YearView } from "./YearView/YearView";
 
-export const Calendar: React.VFC = (props) => {
+export const Calendar: React.VFC<CalendarType> = ({ date, onSelect }) => {
   let [today] = useState(new Date());
   let [viewDate, setViewDate] = useState(new Date());
-  let [selectedDate, setSelectedDate] = useState();
+  let [selectedDate, setSelectedDate] = useState<Date | string>();
   let [currentView, setCurrentView] = useState<ViewKind>("DATE_VIEW");
+
+  /**
+   * 同時設定自身 component 和外部元件傳進來的值，
+   * 目的是當自身 component設定值時，能觸發外部元件的值跟自身元件一樣
+   */
+  function toSelectDate(val: Date | string) {
+    setSelectedDate(val);
+    onSelect(
+      val instanceof Date ? Intl.DateTimeFormat("fr-CA").format(val) : ""
+    );
+  }
 
   function previousMonth() {
     setViewDate(new Date(viewDate.setMonth(viewDate.getMonth() - 1)));
@@ -107,7 +119,7 @@ export const Calendar: React.VFC = (props) => {
         })}`}
         viewDate={viewDate}
         selectedDate={selectedDate}
-        toSelectDate={setSelectedDate}
+        toSelectDate={toSelectDate}
         today={today}
         setPrevView={setPrevView}
         setNextView={setNextView}
